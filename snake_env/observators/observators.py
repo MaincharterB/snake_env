f6f737040e2da3b553dd106a71d7d68c0615c901
Artem Zdanevych <zdanya5532@gmail.com>
2025-02-08 09:56:14 +0100

    V 0.1_release
    * Refactor SimpleSnakeEnv
    * BasicObservator for improved structure and functionality

--- snake_env/observators/observators.py
+++ snake_env/observators/observators.py
@@ -16,8 +16,17 @@ class BasicObservator(Observator):
         
     def get_observation(self):
         free_space = np.zeros((self.memory_manager['mapsize'][0], self.memory_manager['mapsize'][1], 3), dtype=np.uint8)
-        free_space[self.memory_manager['snake_positions'][0][0], self.memory_manager['snake_positions'][0][1]] = [0, 255, 0]
+        snake_head_x, snake_head_y = self.memory_manager['snake_positions'][0]
+        if snake_head_x == self.memory_manager['mapsize'][0]:
+            snake_head_x = 0
+        if snake_head_y == self.memory_manager['mapsize'][1]:
+            snake_head_y = 0
+        free_space[snake_head_x, snake_head_y] = [0, 255, 0]    
         for x, y in self.memory_manager['snake_positions'][1:]:
+            if x == self.memory_manager['mapsize'][0]:
+                x = 0
+            if y == self.memory_manager['mapsize'][1]:
+                y = 0
             free_space[x, y] = [0, 0, 255]
         for x, y in self.memory_manager['food_positions'] if self.memory_manager['food_positions'] else []:
             free_space[x, y] = [255, 0, 0]
